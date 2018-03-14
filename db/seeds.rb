1e3fd9b841db960fbcd69595bf068ae20efbf680
kylesallette <kylesallette@gmail.com>
2018-03-14 16:07:37 -0600

    all csv files now working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
 require 'csv'
 require_relative '../app/models/merchant.rb'
+require_relative '../app/models/invoice.rb'
+require_relative '../app/models/item.rb'
 
 
 CSV.foreach('data/merchants.csv', headers: true, header_converters: :symbol, converters: :numeric) do |row|
   Merchant.create(row.to_hash)
+end
+
+CSV.foreach('data/invoices.csv', headers: true, header_converters: :symbol, converters: :numeric) do |row|
+  Invoice.create(row.to_hash)
+end
+
+CSV.foreach('data/items.csv', headers: true, header_converters: :symbol, converters: :numeric) do |row|
+  Item.create(id:          row[:id],
+              name:        row[:name],
+              description: row[:description],
+              unit_price:  row[:unit_price],
+              merchant_id: row[:merchant_id],
+              created_at:  row[:created_at],
+              updated_at:  row[:updated_at],
+              image:       "../public/images/ruby.png")
 end
