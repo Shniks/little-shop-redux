c79e75980c9086a02f6f7f62fda41752ca786f89
nergdvnlt <tylerlundgren@yahoo.com>
2018-03-14 12:59:01 -0600

    Invoice iteration

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -1,3 +1,7 @@
+# Provides access to the routes for our app.
 class LittleShopApp < Sinatra::Base
-
+  get '/invoices' do
+    invoices = Invoice.all
+    erb :'invoices/index', :locals => { :invoices => invoices }
+  end
 end
