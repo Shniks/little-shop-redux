bcf758797807ce32cc982ffa49f879ef369a03a8
Lihkin <shniks@gmail.com>
2018-03-14 16:27:45 -0600

    Refactoring rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
-ENV["RACK_ENV"] ||= "test"
-#require 'bundler'
-#Bundler.require(:default, :test)
+ENV["RACK_ENV"]
+
+require 'bundler'
+Bundler.require(:default, :test)
 
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec'
