00dac5dfa43bf2bb265c2a9268038be080ad339a
Lihkin <shniks@gmail.com>
2018-03-14 12:48:12 -0600

    Completing Merchant model.

---
+++ spec/models/merchant_spec.rb
@@ -0,0 +1,14 @@
+require './app/models/merchant.rb'
+
+RSpec.describe Merchant do
+  describe 'Class Methods' do
+
+  end
+
+  describe 'Validate fields' do
+    it 'is invalid to not have a name' do
+      merchant = Merchant.new()
+      expect(merchant).to_not be_valid
+    end
+  end
+end
