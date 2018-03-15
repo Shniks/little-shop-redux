7641726fefe2e616db0e1a39f29b18b127a29f21
Lihkin <shniks@gmail.com>
2018-03-15 00:28:07 -0600

    Adding views for edit, new and show erb files.

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -1,17 +1,47 @@
-# Provides access to the routes for our app.
 class LittleShopApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
+
+  get '/merchants/new' do
+    erb :'merchants/new'
+  end
+
+  post '/merchants' do
+    merchant = Merchant.new(params[:merchant])
+    merchant.save
+    redirect '/merchants'
+  end
 
   get '/merchants' do
-    @merchants = Merchant.all
-    erb :"merchants/index"
+    merchants = Merchant.all
+    erb :'merchants/index', locals: {merchants: merchants}
   end
 
-  get "/items" do
-    items = Item.all
-    erb :"items/index", locals: {items: items}
+  get '/merchants/:id' do
+    merchant = Merchant.find(params[:id])
+    erb :'merchants/show', locals: {merchant: merchant}
   end
-  
+
+  get '/merchants/:id/edit' do
+    merchant = Merchant.find(params[:id])
+    erb :'merchants/edit', locals: {merchant: merchant}
+  end
+
+  put '/merchants/:id' do |id|
+    Merchant.update(id.to_i, params[:merchant])
+    redirect "/merchants/#{params[:merchant][:id]}"
+  end
+
+  delete '/merchants/:id' do |id|
+    Merchant.destroy(id.to_i)
+    redirect '/merchants'
+  end
+
+  get '/items' do
+    items = Item.all
+    erb :'items/index', locals: {items: items}
+  end
+
   get '/invoices' do
     invoices = Invoice.all
     erb :'invoices/index', :locals => { :invoices => invoices }
