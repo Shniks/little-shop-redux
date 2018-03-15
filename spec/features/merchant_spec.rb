855dd5aed29713c5f3ca39f58a0515e2469e1e8c
Lihkin <shniks@gmail.com>
2018-03-15 00:40:45 -0600

    Renamed merchant test file under features.

--- spec/features/merchant_spec.rb
+++ spec/features/merchant_spec.rb
@@ -1,7 +1,11 @@
-describe 'user vists merchant index' do
-  it 'it sees the header' do
+describe 'user visits merchant index' do
+  it 'it sees the attribute' do
     Merchant.create(name: "Tyler")
     visit '/merchants'
     expect(page).to have_content("Tyler")
   end
+
+
+
+
 end
