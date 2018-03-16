15e1e9e65795e43e2da3fc22414a65c0c303a1fe
nergdvnlt <tylerlundgren@yahoo.com>
2018-03-15 20:28:09 -0600

    Deleting useless controller tests.

--- spec/controllers/invoice_path_spec.rb
+++
@@ -1,8 +0,0 @@
-describe 'Invoice pathways' do
-  it 'Returns ok status for index' do
-    Invoice.create(customer_id: 8, merchant_id: 88, status: 'pending')
-    visit '/invoices'
-    expect(page).to have_content('pending')
-    # expect(page).to have_http_status(200)
-  end
-end
