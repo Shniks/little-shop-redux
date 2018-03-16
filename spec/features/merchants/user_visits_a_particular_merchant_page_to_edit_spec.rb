e4db434345b5a6a42d2a9736c6f27d9f91fd1301
Lihkin <shniks@gmail.com>
2018-03-15 18:18:21 -0600

    Moved features files into their respective folders. Fixed merchant features test issues.

--- spec/features/merchants/user_visits_a_particular_merchant_page_to_edit_spec.rb
+++ spec/features/merchants/user_visits_a_particular_merchant_page_to_edit_spec.rb
@@ -8,18 +8,10 @@ describe 'As a user' do
       expect(page).to have_content('Items')
       expect(page).to have_content('Merchants')
     end
-
-    it 'I can see content in the body' do
+    
+    it 'I can see input box to edit merchant' do
       merchant = Merchant.create(name: 'Tyler')
       visit "/merchants/#{merchant.id}/edit"
-
-      expect(page).to have_content(merchant.name)
-      expect(page).to have_content(merchant.id)
-    end
-
-    it 'I can see input box to edit merchant' do
-      Merchant.create(name: 'Tyler')
-      visit '/merchants/1/edit'
       fill_in("merchant[name]", :with => 'Sergio')
 
       click_button('Submit')
