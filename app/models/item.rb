be1226af9faa9e995d08adbd8cf92d48bac7865b
Lihkin <shniks@gmail.com>
2018-03-16 17:53:25 -0600

    Established relationship between item and invoice_item.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,7 @@
 class Item < ActiveRecord::Base
-belongs_to :merchant
-validates_presence_of :name
-validates_presence_of :description
-validates_presence_of :unit_price
-
-
+  belongs_to :merchant
+  belongs_to :invoice_item
+  validates_presence_of :name
+  validates_presence_of :description
+  validates_presence_of :unit_price
 end
