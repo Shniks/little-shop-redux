7e1599787454f1c475e11953d1d6bcdd8da1d11b
kylesallette <kylesallette@gmail.com>
2018-03-16 18:43:38 -0600

    tweaking method

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ validates_presence_of :description
 validates_presence_of :unit_price
 
   def self.average_price
-    average(:unit_price).to_f.round(2)
+    (average(:unit_price).to_f / 100).round(2)
   end
 
   def self.total_items
