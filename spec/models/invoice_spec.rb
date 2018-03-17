50387a10cbf4cc68b358d601676ae3f4ef0d8f4a
nergdvnlt <tylerlundgren@yahoo.com>
2018-03-17 12:09:09 -0600

    Adding model test to total price functionality.

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,7 +1,17 @@
 require './app/models/invoice.rb'
+require './app/models/invoice_item.rb'
+require './app/models/item.rb'
 
 RSpec.describe Invoice do
   describe 'Class Methods' do
+    it 'Calculates the total_price for the invoice' do
+      invoice_1 = Invoice.create!(customer_id: 28, merchant_id: 8, status: 'pending')
+      item_1 = Item.create!(name: 'mjolnir', description: 'hammer', unit_price: 555, merchant_id: 8)
+      invoice_item_1 = invoice_1.invoice_items.create!(item_id: 1, invoice_id: 1, quantity: 666, unit_price: 555)
+      item_2 = Item.create!(name: 'Cup', description: 'container', unit_price: 444, merchant_id: 8)
+      invoice_item_2 = invoice_1.invoice_items.create!(item_id: 2, invoice_id: 1, quantity: 108, unit_price: 444)
+
+      expect(Invoice.total_price).to eq(999)
   end
 
   describe 'Validate fields' do
