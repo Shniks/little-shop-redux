70a2df8569e2496969b06415bca8f5d3e0b44708
kylesallette <kylesallette@gmail.com>
2018-03-16 18:34:00 -0600

    adding new test for items average

--- spec/features/items/user_sees_all_items_analitics_dashboard_spec.rb
+++ spec/features/items/user_sees_all_items_analitics_dashboard_spec.rb
@@ -40,8 +40,29 @@
 
         visit '/items-dashboard'
 
-        save_and_open_page
+
         expect(page).to have_content(20.0)
+        expect(page).to have_content(3)
+      end
+    end
+  end
+
+  describe "User visits items dashboard"  do
+    context "Visits [/items-dashboard]"  do
+      it "They see average count of items"  do
+        Item.create(name: "toy", description: "Its fun", unit_price: 10, merchant_id: 1, image: "somelink")
+        Item.create(name: "toy toy toy", description: "something", unit_price: 20, merchant_id: 1, image: "somelink")
+        Item.create(name: "slime", description: "watch", unit_price: 30, merchant_id: 1, image: "somelink")
+        Item.create(name: "suoer", description: "Irunning", unit_price: 45.5, merchant_id: 1, image: "somelink")
+        Item.create(name: "boy", description: "yay", unit_price: 340, merchant_id: 1, image: "somelink")
+        Item.create(name: "spider", description: "run", unit_price: 124, merchant_id: 1, image: "somelink")
+        Item.create(name: "roach", description: "oh no", unit_price: 134, merchant_id: 1, image: "somelink")
+
+        visit '/items-dashboard'
+
+
+        expect(page).to have_content(100.5)
+        expect(page).to have_content(7)
       end
     end
   end
