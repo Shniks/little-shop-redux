c812822c26725b458028de4de4f2d2999b967454
kylesallette <kylesallette@gmail.com>
2018-03-16 18:44:07 -0600

    adding more

--- spec/features/items/user_sees_all_items_analitics_dashboard_spec.rb
+++ spec/features/items/user_sees_all_items_analitics_dashboard_spec.rb
@@ -34,9 +34,9 @@
   describe "User visits items dashboard"  do
     context "Visits [/items-dashboard]"  do
       it "They see average count of items"  do
-        Item.create(name: "fun toy", description: "Its fun", unit_price: 20, merchant_id: 1, image: "somelink")
-        Item.create(name: "fun toy", description: "Its fun", unit_price: 20, merchant_id: 1, image: "somelink")
-        Item.create(name: "toy", description: "fun", unit_price: 20, merchant_id: 1, image: "somelink")
+        Item.create(name: "fun toy", description: "Its fun", unit_price: 2000, merchant_id: 1, image: "somelink")
+        Item.create(name: "fun toy", description: "Its fun", unit_price: 2000, merchant_id: 1, image: "somelink")
+        Item.create(name: "toy", description: "fun", unit_price: 2000, merchant_id: 1, image: "somelink")
 
         visit '/items-dashboard'
 
@@ -50,18 +50,18 @@
   describe "User visits items dashboard"  do
     context "Visits [/items-dashboard]"  do
       it "They see average count of items"  do
-        Item.create(name: "toy", description: "Its fun", unit_price: 10, merchant_id: 1, image: "somelink")
-        Item.create(name: "toy toy toy", description: "something", unit_price: 20, merchant_id: 1, image: "somelink")
-        Item.create(name: "slime", description: "watch", unit_price: 30, merchant_id: 1, image: "somelink")
-        Item.create(name: "suoer", description: "Irunning", unit_price: 45.5, merchant_id: 1, image: "somelink")
-        Item.create(name: "boy", description: "yay", unit_price: 340, merchant_id: 1, image: "somelink")
-        Item.create(name: "spider", description: "run", unit_price: 124, merchant_id: 1, image: "somelink")
-        Item.create(name: "roach", description: "oh no", unit_price: 134, merchant_id: 1, image: "somelink")
+        Item.create(name: "toy", description: "Its fun", unit_price: 1000, merchant_id: 1, image: "somelink")
+        Item.create(name: "toy toy toy", description: "something", unit_price: 2000, merchant_id: 1, image: "somelink")
+        Item.create(name: "slime", description: "watch", unit_price: 3000, merchant_id: 1, image: "somelink")
+        Item.create(name: "suoer", description: "Irunning", unit_price: 4500.5, merchant_id: 1, image: "somelink")
+        Item.create(name: "boy", description: "yay", unit_price: 34000, merchant_id: 1, image: "somelink")
+        Item.create(name: "spider", description: "run", unit_price: 12400, merchant_id: 1, image: "somelink")
+        Item.create(name: "roach", description: "oh no", unit_price: 13400, merchant_id: 1, image: "somelink")
 
         visit '/items-dashboard'
+         save_and_open_page
 
-
-        expect(page).to have_content(100.5)
+        expect(page).to have_content(100.43)
         expect(page).to have_content(7)
       end
     end
