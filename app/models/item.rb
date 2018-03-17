77437ade9f275ebfab38fb193ef84f6968e56793
kylesallette <kylesallette@gmail.com>
2018-03-16 18:29:13 -0600

    adding items methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,13 @@ validates_presence_of :name
 validates_presence_of :description
 validates_presence_of :unit_price
 
+  def self.average_price
+    average(:unit_price).to_f.round(2)
+  end
+
+  def self.total_items
+    Item.all.count
+  end
+
 
 end
