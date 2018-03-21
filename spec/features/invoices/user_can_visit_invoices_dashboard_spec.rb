964c422e09e5a942c1c9177af1acdbdeb417a711
Lihkin <shniks@gmail.com>
2018-03-21 08:24:03 -0600

    Refactoring invoices dashboard.

--- spec/features/invoices/user_can_visit_invoices_dashboard_spec.rb
+++ spec/features/invoices/user_can_visit_invoices_dashboard_spec.rb
@@ -1,5 +1,5 @@
 describe 'As a User' do
-  context 'I visit the merchants dashboard' do
+  context 'I visit the invoices dashboard' do
     before(:each) do
       @merchant1 = Merchant.create(id: 1, name: 'Tyler')
       @merchant2 = Merchant.create(id: 2, name: 'Nikhil')
@@ -9,7 +9,7 @@ describe 'As a User' do
       @invoice_item_1 = @invoice_1.invoice_items.create!(item_id: 1, invoice_id: 1, quantity: 666, unit_price: 555)
       @invoice_item_2 = @invoice_1.invoice_items.create!(item_id: 2, invoice_id: 1, quantity: 108, unit_price: 444)
 
-      @invoice_2 = Invoice.create!(customer_id: 50, merchant_id: 2, status: 'pending')
+      @invoice_2 = Invoice.create!(customer_id: 50, merchant_id: 2, status: 'shipped')
       @item_3 = Item.create!(name: 'mjolnir', description: 'hammer', unit_price: 20, merchant_id: 2)
       @item_4 = Item.create!(name: 'Cup', description: 'container', unit_price: 55, merchant_id: 2)
       @invoice_item_3 = @invoice_2.invoice_items.create!(item_id: 1, invoice_id: 2, quantity: 200, unit_price: 20)
@@ -20,33 +20,35 @@ describe 'As a User' do
       DatabaseCleaner.clean
     end
 
-    it 'I can see merchants on the page' do
-      visit '/merchants-dashboard'
+    it 'I can see invoices data on the page' do
+      visit '/invoices-dashboard'
 
-      expect(page).to have_content('Tyler')
-      expect(page).to have_content('Nikhil')
+      expect(page).to have_content('Status Percentages')
+      expect(page).to have_content('Unit Prices')
+      expect(page).to have_content('Quantity')
+      expect(page).to have_content('Highest Invoice')
+      expect(page).to have_content('Lowest Invoice')
     end
 
-    it 'I can see total number of items for a merchant' do
-      visit '/merchants-dashboard'
+    it 'I can see status percentages of invoices' do
+      visit '/invoices-dashboard'
 
-      expect(page).to have_content('Tyler')
-      expect(page).to have_content('Item Count: 2')
-      expect(page).to have_content(@merchant1.name)
+      expect(page).to have_content('Pending: 50%')
+      expect(page).to have_content('Shipped: 50%')
     end
 
-    it 'I can see average item price for a merchant' do
-      visit '/merchants-dashboard'
+    it 'I can see unit prices for particular invoice' do
+      visit '/invoices-dashboard'
 
-      expect(page).to have_content('Tyler')
-      expect(page).to have_content('499.5')
+      expect(page).to have_content('1')
+      expect(page).to have_content('2')
     end
 
-    it 'I can see the total cost of items on merchant dashboard' do
-      visit '/merchants-dashboard'
+    it 'I can see the quantity for a particular invoice' do
+      visit '/invoices-dashboard'
 
-      expect(page).to have_content('Tyler')
-      expect(page).to have_content('999.0')
+      expect(page).to have_content('1')
+      expect(page).to have_content('2')
     end
   end
 end
